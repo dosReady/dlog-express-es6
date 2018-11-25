942e692dd3eb74136d7ee91d39fee711f29b44f3
도오성 <dos@dooseong-ui-MacBook-Pro.local>
2018-11-25 15:36:06 +0900

    - 댓글 디자인을 잘못 한거같다.  다시계획해서 하자.

---
+++ frontend/src/components/comment/CommentContainer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="comment-container">
+      <div class="comment-wrap" v-for="(item, index) in data" :key="index">
+        <div class="comment-top">
+          <span>등록자</span>
+          <span>{{item.comment_update_date}}</span>
+        </div>
+        <div class="comment-body">
+          <p>{{item}}</p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CommentContainer',
+  props: {
+    data: Array
+  },
+  data () {
+    return {}
+  },
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.commponent-container {
+
+}
+</style>
--- frontend/src/components/logline/LogLineDetailForm.vue
+++ frontend/src/components/logline/LogLineDetailForm.vue
@@ -9,21 +9,15 @@
         </div>
         <div class="logline-content markdown-body" v-html="compiledMarkdown"></div>
         <comment-container :data="comments"></comment-container>
-        <div class="comment-input">
-          <textarea v-model="inputComment.comment_content" placeholder="댓글을 입력해주세요."></textarea>
-          <div class="btn-wrap">
-            <button class="btn btn-default" @click="add_comment">댓글 저장</button>
-          </div>
-        </div>
+        <comment-component></comment-component>
     </div>
-    <div class="page-bottom"></div>
   </div>
 </template>
 
 <script>
 import marked from 'marked'
-import Pagnation from '@/components/Pagnation'
-import CommentContainer from '@/components/CommentContainer'
+import CommentComponent from '@/components/comment/CommentComponent'
+import CommentContainer from '@/components/comment/CommentContainer'
 export default {
   name: 'LogLineDetailForm',
   data () {
@@ -37,7 +31,7 @@ export default {
     }
   },
   components: {
-    Pagnation,
+    CommentComponent,
     CommentContainer
   },
   async beforeCreate () {
@@ -48,42 +42,7 @@ export default {
       this.data = logline.data
     }
   },
-  methods: {
-    async add_comment () {
-      if (confirm('댓글을 저장하시겠습니까?')) {
-        try {
-          await this.$http.post('/api/comment/add', {data: this.inputComment})
-          alert('저장되었습니다.')
-          this.init_comment()
-        } catch (error) {
-          console.log(error)
-          alert('저장 처리중 오류가 발생했습니다.')
-        }
-      }
-    },
-    async delete_comment (paramsSeq) {
-      if (confirm('댓글을 삭제하시겠습니까?')) {
-        try {
-          await this.$http.post('/api/comment/delete', {seq: paramsSeq})
-          alert('삭제되었습니다.')
-          this.init_comment()
-        } catch (error) {
-          console.log(error)
-          alert('삭제 처리중 오류가 발생했습니다.')
-        }
-      }
-    },
-    async init_comment () {
-      try {
-        const {data} = await this.$http.post('/api/comment/list', {id: this.$route.params.id})
-        this.comments = data
-        this.inputComment.comment_content = ''
-      } catch (error) {
-        console.log(error)
-        alert('댓글 조회중 오류가 발생했습니다.')
-      }
-    }
-  },
+  methods: {},
   computed: {
     compiledMarkdown () {
       let content = ''
@@ -128,48 +87,5 @@ export default {
 .update-date {
   font-size: .875rem;
   color: #8aa6c1;
-}
-.comment-container {
-  border-bottom: 1px solid #e7edf3;
-  margin-bottom: 1rem;
-  .comment-top {
-    display: flex;
-    align-items: center;
-    height: 2rem;
-    h4 {
-      margin-right: 1rem;
-    }
-  }
-  .comment-content {
-    p {
-      word-break: break-all;
-    }
-    .btn-wrap {
-    margin-top: 1rem;
-    display: flex;
-    justify-content: flex-end;
-    margin-bottom: 1rem;
-    }
-  }
-}
-.comment-pagnation {
-  ul {
-    display: flex;
-    list-style: none;
-    justify-content: center;
-    li {
-      margin-right: 2rem;
-    }
-  }
-}
-.comment-input {
-  display: flex;
-  flex-direction: column;
-  margin-top: 2rem;
-  .btn-wrap {
-    margin-top: 1rem;
-    display: flex;
-    justify-content: flex-end;
-  }
 }
 </style>
